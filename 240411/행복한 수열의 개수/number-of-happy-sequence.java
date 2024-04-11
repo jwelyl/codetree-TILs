320758a0805623b153259c29af3fe0582e5059cd
koreii <61617029+jwelyl@users.noreply.github.com>
2024-04-11 11:53:37 +0900

    Title: 행복한 수열의 개수, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240411/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240411/행복한 수열의 개수/number-of-happy-sequence.java
@@ -25,46 +25,51 @@ public class Main {
             }
         }
 
-        //  n개의 행에 대해 행복한 수열인지 확인
-        for(int row = 0; row < n; row++) {
-            int serial = 1;
-            int same = nums[row][0];
+        if(m != 1) {
+            //  n개의 행에 대해 행복한 수열인지 확인
+            for(int row = 0; row < n; row++) {
+                int serial = 1;
+                int same = nums[row][0];
 
-            for(int col = 1; col < n; col++) {
-                if(nums[row][col] == same) {
-                    serial++;
+                for(int col = 1; col < n; col++) {
+                    if(nums[row][col] == same) {
+                        serial++;
 
-                    if(serial == m) {   //  row번째 행이 행복한 수열일 경우
-                        ans++;
-                        break;
+                        if(serial == m) {   //  row번째 행이 행복한 수열일 경우
+                            ans++;
+                            break;
+                        }
+                    }
+                    else {
+                        same = nums[row][col];
+                        serial = 1;
                     }
                 }
-                else {
-                    same = nums[row][col];
-                    serial = 1;
+            }
+
+            //  n개의 열에 대해 행복한 수열인지 확인
+            for(int col = 0; col < n; col++) {
+                int serial = 1;
+                int same = nums[0][col];
+
+                for(int row = 1; row < n; row++) {
+                    if(nums[row][col] == same) {
+                        serial++;
+
+                        if(serial == m) {   //  col번째 열이 행복한 수열일 경우
+                            ans++;
+                            break;
+                        }
+                    }
+                    else {
+                        same = nums[row][col];
+                        serial = 1;
+                    }
                 }
             }
         }
-
-        //  n개의 열에 대해 행복한 수열인지 확인
-        for(int col = 0; col < n; col++) {
-            int serial = 1;
-            int same = nums[0][col];
-
-            for(int row = 1; row < n; row++) {
-                if(nums[row][col] == same) {
-                    serial++;
-
-                    if(serial == m) {   //  col번째 열이 행복한 수열일 경우
-                        ans++;
-                        break;
-                    }
-                }
-                else {
-                    same = nums[row][col];
-                    serial = 1;
-                }
-            }
+        else {
+            ans = 2 * n;
         }
 
         System.out.println(ans);
