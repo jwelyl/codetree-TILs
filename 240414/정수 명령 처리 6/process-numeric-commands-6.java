339881ef3628a238c801497108bc63ad20bfaed1
koreii <61617029+jwelyl@users.noreply.github.com>
2024-04-14 18:43:08 +0900

    Title: 정수 명령 처리 6, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240414/정수 명령 처리 6/process-numeric-commands-6.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+    private static StringTokenizer tokens;
+
+    private static final PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    private static int n;
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            tokens = new StringTokenizer(br.readLine());
+
+            String cmd = tokens.nextToken();
+
+            if(cmd.equals("push")) {
+                int value = Integer.parseInt(tokens.nextToken());
+
+                pq.offer(-value);
+            } else if(cmd.equals("pop")) {
+                int value = -pq.poll();
+
+                sb.append(value).append("\n");
+            } else if(cmd.equals("size")) {
+                sb.append(pq.size()).append("\n");
+            } else if(cmd.equals("empty")) {
+                int isEmpty = pq.isEmpty() ? 1 : 0;
+
+                sb.append(isEmpty).append("\n");
+            } else if(cmd.equals("top")) {
+                sb.append(pq.peek()).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
