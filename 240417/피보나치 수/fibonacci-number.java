7d6fda3650e38a9ed4f8ba5cdb30fbae8b0d805a
koreii <61617029+jwelyl@users.noreply.github.com>
2024-04-17 10:32:41 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240417/피보나치 수/fibonacci-number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamREader(System.in));
+    
+    private static final int[] fibo = new int[46];
+
+    private static int n = 0;
+
+    public static void main(String[] args) {
+        fibo[1] = 1;
+        fibo[2] = 1;
+
+        for(int i = 3; i <= 45; i++)
+            fibo[i] = fibo[i - 2] + fibo[i - 1];
+
+        n = Integer.parseInt(br.readLine());
+
+        System.out.println(fibo[n]);
+    }   //  main-end
+}   //  Main-class-end
