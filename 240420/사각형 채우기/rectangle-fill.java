ef8982337ba3b77e848e83d8710be420631e1c49
koreii <61617029+jwelyl@users.noreply.github.com>
2024-04-20 15:50:57 +0900

    Title: 사각형 채우기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240420/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final int MOD = 10_007;
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static int n;
+    private static int[] dp;
+    
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++)
+            dp[i] = (dp[i - 1] % MOD + dp[i - 2] % MOD) % MOD;
+
+        System.out.println(dp[n]);
+    }   //  main-end
+}   //  Main-class-end
