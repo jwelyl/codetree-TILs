da84beb9bf30c29abd4bab06be69e9d504fb51ef
koreii <61617029+jwelyl@users.noreply.github.com>
2024-04-20 16:02:25 +0900

    Title: 사각형 채우기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240420/사각형 채우기/rectangle-fill.java
+++ 240420/사각형 채우기/rectangle-fill.java
@@ -15,7 +15,9 @@ public class Main {
         dp = new int[n + 1];
 
         dp[1] = 1;
-        dp[2] = 2;
+
+        if(n >= 2)
+            dp[2] = 2;
 
         for(int i = 3; i <= n; i++)
             dp[i] = (dp[i - 1] % MOD + dp[i - 2] % MOD) % MOD;
