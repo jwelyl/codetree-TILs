4469c142723129a2653bc651cf3db641c89fbbfe
koreii <61617029+jwelyl@users.noreply.github.com>
2024-04-28 16:36:52 +0900

    Title: 두 수의 합, Time: 470ms, Memory: 25MB, Status: Passed - CodeTree

--- 240428/두 수의 합/sum-of-two-num.java
+++ 240428/두 수의 합/sum-of-two-num.java
@@ -7,7 +7,8 @@ public class Main {
 
     private static int n;   //  수 개수
     private static int k;   //  두 수 합 k
-    public static final List<Integer> nums = new ArrayList<Integer>();    //  수 목록
+    private static final List<Integer> nums = new ArrayList<Integer>();    //  수 목록
+    private static final Map<Integer, Integer> map = new HashMap<>();      //  Key : 수, Value : 해당 수의 개수
 
     private static long ans = 0L;
     
@@ -20,52 +21,18 @@ public class Main {
         for(int i = 0; i < n; i++)
             nums.add(Integer.parseInt(tokens.nextToken()));
 
-        Collections.sort(nums);
-
-        for(int i = 0; i < n - 1; i++) {
+        for(int i = 0; i < n; i++) {
             int num = nums.get(i);
             int target = k - num;
 
-            if(target >= num)
-                ans += countOccurrences(target, i + 1, n - 1);
+            //  이전 수 중에 target이 존재할 경우
+            if(map.containsKey(target))
+                ans += map.get(target);
+
+            //  이전 수 중에 존재했던 num 개수 + 1
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         System.out.println(ans);
-    }   //  main-end
-
-    public static int countOccurrences(int target, int start, int end) {
-        int firstIndex = findFirstIndex(target, start, end);
-        int lastIndex = findLastIndex(target, start, end);
-        
-        if (firstIndex == -1 || lastIndex == -1) 
-            return 0;
-        
-        return lastIndex - firstIndex + 1;
-    }
-
-    private static int findFirstIndex(int target, int start, int end) {
-        int index = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums.get(mid) >= target) {
-                if (nums.get(mid) == target) index = mid;
-                end = mid - 1;
-            } else
-                start = mid + 1;
-        }
-        return index;
-    }
-
-    private static int findLastIndex(int target, int start, int end) {
-        int index = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums.get(mid) <= target) {
-                if (nums.get(mid) == target) index = mid;
-                start = mid + 1;
-            } else
-                end = mid - 1;
-        }
-        return index;
-    }                                                                                                                                                                                                                                                                                                                                                               
+    }   //  main-end                                                                                                                                                                                                                                                                                                                           
 }   //  Main-class-end
