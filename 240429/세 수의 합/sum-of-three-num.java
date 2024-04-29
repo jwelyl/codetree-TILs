ef04855ba5337ac4cfb86b701e6ed14d7288c8ff
koreii <61617029+jwelyl@users.noreply.github.com>
2024-04-29 16:28:01 +0900

    Title: 세 수의 합, Time: 263ms, Memory: 20MB, Status: Passed - CodeTree

--- 240429/세 수의 합/sum-of-three-num.java
+++ 240429/세 수의 합/sum-of-three-num.java
@@ -8,7 +8,8 @@ public class Main {
     private static int n;   //  수 개수
     private static int k;   //  세 수 합 k
 
-    private static final List<Integer> nums = new ArrayList<Integer>();      //  수
+    private static final List<Integer> nums = new ArrayList<>();
+    private static final Map<Integer, Integer> numMap = new HashMap<>();      //  Key : 수, Value : 수의 등장 횟수
     private static long ans = 0L;   //  세 수 합 k 되는 조합 개수
 
     public static void main(String[] args) throws IOException {
@@ -17,72 +18,32 @@ public class Main {
         k = Integer.parseInt(tokens.nextToken());
 
         tokens = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++)
-            nums.add(Integer.parseInt(tokens.nextToken()));
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(tokens.nextToken());
+            nums.add(num);
 
-        Collections.sort(nums); //  오름차순 정렬
+            numMap.put(num, numMap.getOrDefault(num, 0) + 1);    //  num의 등장 횟수 증가
+        }
 
-        for(int i = 0; i < n - 2; i++) {
-            for(int j = i + 1; j < n - 1; j++) {
-                int num1 = nums.get(i);
-                int num2 = nums.get(j);
+        for(int i = 0; i < n; i++) {
+            int num1 = nums.get(i); //  첫 번째 수
+
+            if(numMap.containsKey(num1)) {
+                numMap.put(num1, numMap.get(num1) - 1); //  등장 횟수 1 감소 (중복해서 세지 않도록)
+
+                if(numMap.get(num1) == 0)   //  더 이상 등장하지 않을 경우
+                    numMap.remove(num1);    //  num1을 해시맵에서 제거
+            }
+        
+            for(int j = 0; j < i; j++) {
+                int num2 = nums.get(j); //  두 번째 수
                 int target = k - num1 - num2;
-
-                if(target >= num2)  //  찾으려는 수가 num2보다 오른쪽에 존재할 경우
-                    ans += countOfNums(target, j + 1, n - 1);
+                
+                if(numMap.containsKey(target))
+                    ans += numMap.get(target);
             }
         }
 
         System.out.println(ans);
     }   //  main-end
-
-    private static int countOfNums(int target, int start, int end) {
-        int startIdx = leftBound(target, start, end);   //  nums에서 target의 가장 왼쪽 index
-        int endIdx = rightBound(target, start, end);    //  nums에서 target의 가장 오른쪽 index
-
-        if(endIdx == -1 || startIdx == -1)  //  둘 중 하나라도 -1이면
-            return 0;   //  target이 존재하지 않음
-
-        return endIdx - startIdx + 1;
-    }
-
-    //  [start, end]에서 가장 처음 나타나는 target의 index
-    private static int leftBound(int target, int start, int end) {
-        int ret = -1;
-
-        while(start <= end) {
-            int mid = (start + end) / 2;
-
-            if(nums.get(mid) >= target) {   //  target보다 mid값이 더 크거나 같을 경우
-                if(nums.get(mid) == target) //  target과 일치할 경우
-                    ret = mid;              //  일단 mid 저장
-
-                end = mid - 1;  //  더 왼쪽 찾아봐야 함
-            }
-            else    //  target보다 mid값이 더 작을 경우 
-                start = mid + 1;    //  더 오른쪽 찾아봐야 함
-        }
-
-        return ret;
-    }
-
-    //  [start, end]에서 가장 나중에 나타나는 target의 index
-    private static int rightBound(int target, int start, int end) {
-        int ret = -1;
-
-        while(start <= end) {
-            int mid = (start + end) / 2;
-            
-            if(nums.get(mid) <= target) {   //  target보다 mid값이 더 작거나 같을 경우
-                if(nums.get(mid) == target) //  target과 일치할 경우
-                    ret = mid;              //  일단 mid 저장
-
-                start = mid + 1;   //  더 오른쪽 찾아봐야 함
-            }
-            else    //  target보다 mid값이 더 클 경우 
-                end = mid - 1;    //  더 왼쪽 찾아봐야 함
-        }
-
-        return ret;
-    }
 }   //  Main-class-end
