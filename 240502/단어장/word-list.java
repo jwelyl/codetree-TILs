b700f373a902bbf3f0285274a47814c7bf0b303a
koreii <61617029+jwelyl@users.noreply.github.com>
2024-05-02 03:39:13 +0900

    Title: 단어장, Time: 769ms, Memory: 39MB, Status: Passed - CodeTree

---
+++ 240502/단어장/word-list.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.Map.*;
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+    
+    private static int n = 0;   //  단어 개수
+    private static final Map<String, Integer> map = new TreeMap<>();
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            String word = br.readLine();
+
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        for(Entry<String, Integer> entry : map.entrySet()) {
+            String word = entry.getKey();
+            int cnt = entry.getValue();
+
+            sb.append(String.format("%s %d\n", word, cnt));
+        }
+
+        System.out.print(sb);
+    }   //  main-end
+}   //  Main-class-end
