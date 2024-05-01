7c4d9c63fe277e2c407109602d5cd3c9d3fd8dd0
koreii <61617029+jwelyl@users.noreply.github.com>
2024-05-01 14:35:33 +0900

    Title: 원소의 합이 0, Time: 2915ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240501/원소의 합이 0/the-sum-of-the-elements-is-0.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+
+  private static final Map<Integer, Integer> map = new HashMap<>();
+  private static int n;
+  private static int[] a, b, c, d;
+
+  private static long ans = 0L;
+
+  public static void main(String[] args) throws IOException {
+    n = Integer.parseInt(br.readLine());
+
+    a = new int[n];
+    b = new int[n];
+    c = new int[n];
+    d = new int[n];
+
+    tokens = new StringTokenizer(br.readLine());
+    for(int i = 0; i < n; i++)
+      a[i] = Integer.parseInt(tokens.nextToken());
+    tokens = new StringTokenizer(br.readLine());
+    for(int i = 0; i < n; i++)
+      b[i] = Integer.parseInt(tokens.nextToken());
+    tokens = new StringTokenizer(br.readLine());
+    for(int i = 0; i < n; i++)
+      c[i] = Integer.parseInt(tokens.nextToken());
+    tokens = new StringTokenizer(br.readLine());
+    for(int i = 0; i < n; i++)
+      d[i] = Integer.parseInt(tokens.nextToken());
+
+
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < n; j++) {
+        for(int k = 0; k < n; k++) {
+          int sum = a[i] + b[j] + c[k];
+          int comp = -sum;
+
+          map.put(comp, map.getOrDefault(comp, 0) + 1);
+        }
+      }
+    }
+
+    for(int i = 0; i < n; i++) {
+      int num = d[i];
+
+      ans += map.getOrDefault(num, 0);
+    }
+
+    System.out.println(ans);
+  }   //  main-end
+}   //  Main-class-end
