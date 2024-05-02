db75c2964a513344e1876eccffe1a7252bb3bb35
koreii <61617029+jwelyl@users.noreply.github.com>
2024-05-03 02:26:06 +0900

    Title: 데이터 비교, Time: 544ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240503/데이터 비교/data-comparison.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+    private static StringTokenizer tokens;
+
+    private static int n;
+    private static int m;
+
+    private static final Set<Integer> set = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        tokens = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++)
+            set.add(Integer.parseInt(tokens.nextToken()));
+        
+        m = Integer.parseInt(br.readLine());
+        tokens = new StringTokenizer(br.readLine());
+        for(int i = 0; i< m; i++) {
+            if(set.contains(Integer.parseInt(tokens.nextToken())))
+                sb.append("1 ");
+            else
+                sb.append("0 ");
+        }
+
+        System.out.print(sb);
+    }   //  main-end
+}   //  Main-class-end
