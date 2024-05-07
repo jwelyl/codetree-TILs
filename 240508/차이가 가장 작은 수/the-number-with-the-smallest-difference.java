171e7064f75d21c095a1501822d884f5d232020e
koreii <61617029+jwelyl@users.noreply.github.com>
2024-05-08 01:53:27 +0900

    Title: 차이가 가장 작은 수, Time: 533ms, Memory: 25MB, Status: Passed - CodeTree

--- 240508/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 240508/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
   private static final int NONE = Integer.MAX_VALUE;
@@ -11,6 +12,7 @@ public class Main {
   private static int n; //  수의 개수
   private static int m; //  두 수의 차이가 m 이상이어야 함
 
+  private static final TreeSet<Integer> treeSet = new TreeSet<>();
   private static int[] nums;  //  주어진 수
   private static int ans = NONE;  //  두 수의 차이가 m 이상이면서 최소인 차이, m 이상인 차이가 없으면 NONE
 
@@ -21,25 +23,21 @@ public class Main {
     m = Integer.parseInt(tokens.nextToken());
 
     nums = new int[n];
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++) {
       nums[i] = Integer.parseInt(br.readLine());
+      treeSet.add(nums[i]);
+    }
 
-    Arrays.sort(nums);
+    for(int i = 0; i < n; i++) {
+      int num = nums[i];
 
-    int start = 0;
-    int end = 1;
+      Integer left = treeSet.floor(num - m);      //  num보다 m 이상 작으면서 가장 큰 수
+      Integer right = treeSet.ceiling(num + m);   //  num보다 m 이상 크면서 가장 작은 수
 
-    while(start < end && end < n) {
-      int diff = nums[end] - nums[start];
+      int leftDiff = left == null ? NONE : num - left;
+      int rightDiff = right == null ? NONE : right - num;
 
-      if(diff >= m) {  //  두 수의 차이가 m 이상이면
-        ans = Math.min(ans, nums[end] - nums[start]); //  두 수의 차이 더 작은 값으로 갱신
-        start++;  //  더 작은 차이가 있나 확인하기 위해 start 키워서 차이 줄여보기
-        if(start == end)  //  만약 start가 end를 따라잡으면 둘이 같은 수가 됨
-          end++;  //  end 더 키워서 서로 다른 수가 되게 하기
-      }
-      else //  두 수 차이가 m 미만이면
-        end++;  //  더 큰 차이가 있나 확인하기 위해 end 키워서 차이 크게 하기
+      ans = Math.min(ans, Math.min(leftDiff, rightDiff));
     }
 
     System.out.println(ans == NONE ? -1 : ans);
