0837b8a86fb7684fd3c54126ff41c9f888da49aa
koreii <61617029+jwelyl@users.noreply.github.com>
2024-05-16 22:26:35 +0900

    Title: 정원 입장은 선착순, Time: 675ms, Memory: 37MB, Status: Passed - CodeTree

--- 240516/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.kt
+++ 240516/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.kt
@@ -8,11 +8,10 @@ class Main {
 
     private var n = 0
 
-    private val personList = ArrayList<Person>()
-    private val pq = PriorityQueue<Person>()
+    private val personList = ArrayList<Person>()    //  빨리 도착하는 순서
+    private val pq = PriorityQueue<Person>()        //  기다리는 사람 pq
 
-    private var nowTime = 0 //  들어갈 수 있는 시간
-    private var idx = 0
+    private var exit = 0 //  들어간 사람이 나오는 시간
 
     private var maxWait = 0 //  최대 기다린 시간
 
@@ -25,32 +24,27 @@ class Main {
             personList.add(Person(it + 1, tokens.nextToken().toInt(), tokens.nextToken().toInt()))
         }
 
-//        println("personList = $personList")
+        personList.add(Person(n + 1, Int.MAX_VALUE, 0))
 
         personList.sortWith(compareBy( {it.arrival}, {it.num} ))   //  도착 시간이 빠른 순으로 정렬, 도착 시간이 같을 경우 번호표 번호가 빠른 순으로 정렬
 
-//        println("personList = $personList")
+        for(idx in 0 .. n) {
+            val person = personList[idx]    // 현재 도착한 사람
+            val num = person.num
+            val arrival = person.arrival
+            val time = person.time
 
-        while(idx < n) {
-            if(personList[idx].arrival >= nowTime) {  //  가장 빨리 도착하는 사람이 들어갈 수 있는 시간 이후에 도착할 경우
-                nowTime = personList[idx].arrival + personList[idx].time    //  들어갈 수 있는 시간 갱신
-                idx++
+            while(arrival > exit && pq.isNotEmpty()) {  //  현재 도착한 사람 도착 시간이 이전 있던 사람 나오는 시간보다 늦고, 먼저 기다리는 사람이 있을 경우
+                val waitPerson = pq.poll()  //  기다리는 사람 중 가장 우선순위 높은 사람이 먼저 들어감
+
+                maxWait = maxOf(maxWait, exit - waitPerson.arrival) //  기다리는 사람이 기다린 시간과 비교
+                exit += waitPerson.time //  기다리는 사람이 나올 시간 계산
             }
-            else {
-                while(idx < n && nowTime >= personList[idx].arrival) //  기다려야 하는 사람들 모두 pq에 넣기
-                    pq.offer(personList[idx++])
 
-                while(pq.isNotEmpty()) {    //  번호표 번호 작은 사람부터 뽑기
-                    val person = pq.poll()
-
-//                    println(person)
-                    val wait = nowTime - person.arrival
-//                    println("wait = $wait")
-
-                    maxWait = maxOf(maxWait, nowTime - person.arrival) //  해당 사람이 기다려야 하는 시간과 비교해서 최대로 기다려야 하는 시간 갱신
-                    nowTime += person.time
-                }
-            }
+            if(arrival > exit)  //  먼저 기다리는 사람 없고, 들어간 사람이 나올 시간보다 늦게 도착한 경우
+                exit = arrival + time   //  내가 들어가고 나온 시간 계산
+            else    //  먼저 기다리는 사람은 없지만 들어간 사람이 나올 시간보다 일찍 도착한 경우
+                pq.offer(person)    //  기다리는 사람에 넣기
         }
 
         println(maxWait)
@@ -59,10 +53,6 @@ class Main {
     private class Person(val num : Int, val arrival : Int, val time : Int) : Comparable<Person> {
         override fun compareTo(other: Person): Int {
             return this.num.compareTo(other.num)
-        }
-
-        override fun toString(): String {
-            return "($num, $arrival, $time)"
         }
     }
 }
