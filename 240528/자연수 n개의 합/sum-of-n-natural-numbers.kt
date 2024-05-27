4e3c0f61915989c6b6daafad3896a81a414139fa
koreii <61617029+jwelyl@users.noreply.github.com>
2024-05-28 01:02:20 +0900

    Title: 자연수 n개의 합, Time: 112ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240528/자연수 n개의 합/sum-of-n-natural-numbers.kt
@@ -0,0 +1,37 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+
+    private var s = 0L
+
+    fun solve() {
+        s = br.readLine().toLong()
+
+        println(parametricSearch())
+    }
+
+    private fun parametricSearch() : Long {
+        var start = 1L
+        var end = 10_000_000_000
+
+        var ans = 0L
+
+        while(start <= end) {
+            val mid = (start + end) / 2
+
+            if(mid * (mid + 1) / 2 <= s) {  //  1부터 mid까지의 합이 s 이하일 경우
+                ans = mid           //  mid는 가능한 n 값임
+                start = mid + 1     //  mid 더 키워보기
+            }
+            else end = mid - 1      //  mid 더 줄여야 함
+        }
+
+        return ans
+    }
+}
+
+fun main() {
+    Main().solve()
+}
