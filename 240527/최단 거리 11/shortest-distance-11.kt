d404eec237786324e2cf3bfda0bb7fc90c33fe37
koreii <61617029+jwelyl@users.noreply.github.com>
2024-05-27 13:45:41 +0900

    Title: 최단 거리 11, Time: 839ms, Memory: 38MB, Status: Passed - CodeTree

--- 240527/최단 거리 11/shortest-distance-11.kt
+++ 240527/최단 거리 11/shortest-distance-11.kt
@@ -1,65 +1,104 @@
 import java.util.*
-import kotlin.math.min
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var n = 0
+    private var m = 0
+
+    private lateinit var graph : Array<ArrayList<Node>>
+    private lateinit var dist : LongArray
+
+    private var a = 0
+    private var b = 0
+
+    fun solve() {
+        tokens = StringTokenizer(br.readLine())
+        n = tokens.nextToken().toInt()
+        m = tokens.nextToken().toInt()
+
+        graph = Array(n + 1) { ArrayList<Node>() }
+        dist = LongArray(n + 1) { Long.MAX_VALUE }
+
+        repeat(m) {
+            tokens = StringTokenizer(br.readLine())
+            val v1 = tokens.nextToken().toInt()
+            val v2 = tokens.nextToken().toInt()
+            val cost = tokens.nextToken().toLong()
+
+            graph[v1].add(Node(v2, cost))
+            graph[v2].add(Node(v1, cost))
+        }
+
+        tokens = StringTokenizer(br.readLine())
+        a = tokens.nextToken().toInt()
+        b = tokens.nextToken().toInt()
+
+        dijkstra()  //  b부터 각 지점까지 최단거리 구하기
+
+        for(i in 1 .. n)
+            graph[i].sortBy { it.v }
+
+        var curV = a
+        var curDist = 0L //  curV까지 누적 거리
+        
+        sb.append("${dist[a]}\n")
+
+        while(curV != b) {
+            sb.append("$curV ")
+
+            for(next in graph[curV]) {
+                val nextV = next.v  //  curV와 연결된 정점
+
+                if(curDist + next.c + dist[nextV] == dist[a]) {   //  curV와 nextV 사이의 간선 길이 + nextV와 b 사이의 거리 = a와 b 사이의 거리이면
+                    curV = nextV    //  다음 정점은 nextV
+                    curDist += next.c
+                    break
+                }
+            }
+        }
+
+        sb.append(curV)
+
+        println(sb)
+    }
+
+    private fun dijkstra() {
+        val pq = PriorityQueue<Node>()
+
+        dist[b] = 0
+        pq.offer(Node(b, dist[b]))
+
+        while(pq.isNotEmpty()) {
+            val cur = pq.poll()
+            val cv = cur.v
+            val cc = cur.c
+
+            if(dist[cv] < cc)
+                continue
+
+            for(next in graph[cv]) {
+                val nv = next.v
+                val nc = cur.c + next.c
+
+                if(nc < dist[nv]) { //  cv를 거쳐서 nv까지 가는게 더 빠를 경우
+                    dist[nv] = nc
+                    pq.offer(Node(nv, dist[nv]))
+                }
+            }
+        }
+    }
+
+    private class Node(val v : Int, val c : Long) : Comparable<Node> {
+        override fun compareTo(other : Node) : Int {
+            return this.c.compareTo(other.c)
+        }
+    }
+}
 
 fun main() {
-    val sc = Scanner(System.`in`)
-    val sb = StringBuilder()
-
-    val n = sc.nextInt()
-    val m = sc.nextInt()
-
-    val graph = List(n) { MutableList(n) { 0 } }
-    repeat(m) {
-        val start = sc.nextInt() - 1
-        val end = sc.nextInt() - 1
-        val dist = sc.nextInt()
-
-        graph[start][end] = dist
-        graph[end][start] = dist
-    }
-
-    val a = sc.nextInt() - 1
-    val b = sc.nextInt() - 1
-
-    val dist = MutableList(n) { 1e9.toInt() }
-    val visited = MutableList(n) { false }
-
-    dist[b] = 0
-
-    for(i in 0 until n) {
-        var minIndex = -1
-
-        for(j in 0 until n) {
-            if(visited[j]) continue
-
-            if(minIndex == -1 || dist[minIndex] > dist[j]) {
-                minIndex = j
-            }
-        }
-
-        visited[minIndex] = true
-
-        for(j in 0 until n) {
-            if(graph[minIndex][j] == 0) continue
-
-            dist[j] = min(dist[j], dist[minIndex] + graph[minIndex][j])
-        }
-    }
-
-    sb.append("${dist[a]}\n")
-    var x = a
-    sb.append("${x+1} ")
-    while(x != b) {
-        for (i in 0 until n) {
-            if(graph[i][x] == 0) continue
-
-            if (dist[i] + graph[i][x] == dist[x]) {
-                x = i
-                break
-            }
-        }
-        sb.append("${x+1} ")
-    }
-
-    println(sb.toString())
+    Main().solve()
 }
