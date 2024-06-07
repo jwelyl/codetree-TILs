27bd0bf1c0a6f0d2fda327b92e538b12508d772c
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-07 19:39:59 +0900

    Title: 문자열 받아 출력, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240607/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(br.readLine());
+  } //  main-end
+} //  Main-class-end
