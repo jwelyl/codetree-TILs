9f2778fc445b5ebe31bfc8487ddc30926e8c1abb
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-07 19:47:16 +0900

    Title: 입력받아 출력 2, Time: 116ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240607/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+  private static int n, m;
+
+  public static void main(String[] args) throws IOException {
+    tokens = new StringTokenizer(br.readLine(), "-");
+    n = Integer.parseInt(tokens.nextToken());
+    m = Integer.parseInt(tokens.nextToken());
+
+    System.out.println(n + "" + m);
+  } //  main-end
+} //  Main-class-end
