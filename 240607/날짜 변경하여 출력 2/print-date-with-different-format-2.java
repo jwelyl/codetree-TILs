2c2148223bc7c21cbdedb56f57886fc6c522726c
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-07 19:45:30 +0900

    Title: 날짜 변경하여 출력 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240607/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+  private static int n, m, l;
+
+  public static void main(String[] args) throws IOException {
+    tokens = new StringTokenizer(br.readLine(), ":");
+    n = Integer.parseInt(tokens.nextToken());
+    m = Integer.parseInt(tokens.nextToken());
+    l = Integer.parseInt(tokens.nextToken());
+
+    System.out.println(l + "." + m + "." + n);
+  } //  main-end
+} //  Main-class-end
