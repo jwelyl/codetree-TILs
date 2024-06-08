7696f5e93944f8b8b60c1277cc1fe0d097f5fc63
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-08 16:39:34 +0900

    Title: 간단한 사칙연산, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240608/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+  private static int a, b;
+
+  public static void main(String[] args) throws IOException {
+    tokens = new StringTokenizer(br.readLine());
+    a = Integer.parseInt(tokens.nextToken());
+    b = Integer.parseInt(tokens.nextToken());
+    
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a / b);
+    System.out.println(a % b);
+  } //  main-end
+} //  Main-class-end
