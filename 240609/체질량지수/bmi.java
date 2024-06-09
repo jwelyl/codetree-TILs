416819ac3b06e14ed357e169b3ec31272b4802ad
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-09 22:22:08 +0900

    Title: 체질량지수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240609/체질량지수/bmi.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+  private static int h, w;
+  private static double b;
+
+  public static void main(String[] args) throws IOException {
+    tokens = new StringTokenizer(br.readLine());
+    h = Integer.parseInt(tokens.nextToken());
+    w = Integer.parseInt(tokens.nextToken());
+
+    b = 10_000.0f * w / h / h;
+
+    System.out.printf("%d\n", b);
+    if(b >= 25)
+        System.out.println("Obesty");
+  } //  main-end
+} //  Main-class-end
