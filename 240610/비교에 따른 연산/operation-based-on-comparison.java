86e9ea95cd9ecaffbb0d3f38d02d37abe73475f2
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-10 01:02:57 +0900

    Title: 비교에 따른 연산, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240610/비교에 따른 연산/operation-based-on-comparison.java
+++ 240610/비교에 따른 연산/operation-based-on-comparison.java
@@ -7,7 +7,7 @@ public class Main {
   private static int a, b;
 
   public static void main(String[] args) throws IOException {
-    tokens = new StringTokenizer(br.readLine();)
+    tokens = new StringTokenizer(br.readLine());
     a = Integer.parseInt(tokens.nextToken());
     b = Integer.parseInt(tokens.nextToken());
 
