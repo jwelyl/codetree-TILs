3cbe09a45b92e43ccd4ae555539240b5983c956f
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-10 01:07:35 +0900

    Title: 살 수 있는 물건, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240610/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+  private static double a, b;
+
+  public static void main(String[] args) throws IOException {
+    // tokens = new StringTokenizer(br.readLine());
+    // a = Integer.parseInt(tokens.nextToken());
+    // b = Integer.parseInt(tokens.nextToken());
+    a = Int.parseInt(br.readLine());
+
+    if(a >= 3000)
+        System.out.println("book");
+    else if(a >= 1000)
+        System.out.println("mask");
+    else
+        System.out.println("no");
+  } //  main-end
+} //  Main-class-end
