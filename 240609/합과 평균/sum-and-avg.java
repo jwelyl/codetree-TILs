dac1264f7507fad8e2ef37aeabc6e14ea5296d63
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-09 22:10:03 +0900

    Title: 합과 평균, Time: 100ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240609/합과 평균/sum-and-avg.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+  private static int a, b;
+
+  public static void main(String[] args) throws IOException {
+    tokens = new StringTokenizer(br.readLine());
+    a = Integer.parseInt(tokens.nextToken());
+    b = Integer.parseInt(tokens.nextToken());
+
+    System.out.printf("%d %.2f\n", a + b, (a + b) / 2.0);
+  } //  main-end
+} //  Main-class-end
