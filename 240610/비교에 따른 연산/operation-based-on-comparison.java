2eb91e0c005bbcf8afc6cad7ebed3ea52920763e
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-10 01:02:25 +0900

    Title: 비교에 따른 연산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240610/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+  private static int a, b;
+
+  public static void main(String[] args) throws IOException {
+    tokens = new StringTokenizer(br.readLine();)
+    a = Integer.parseInt(tokens.nextToken());
+    b = Integer.parseInt(tokens.nextToken());
+
+    if(a >= b)
+        System.out.println(a * b);
+    else
+        System.out.println(b / a);
+  } //  main-end
+} //  Main-class-end
