67b5db8ecc2aea5372cf0cf45a74d6f192cc2a9b
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-12 07:43:32 +0900

    Title: 비교 연산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240612/비교 연산/comparison-operator.kt
@@ -0,0 +1,22 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+
+var a = 0
+var b = 0
+
+fun main() {
+    tokens = StringTokenizer(br.readLine())
+
+    a = tokens.nextToken().toInt()
+    b = tokens.nextToken().toInt()
+
+    println(if(a >= b) 1 else 0)
+    println(if(a > b) 1 else 0)
+    println(if(a =< b) 1 else 0)
+    println(if(a < b) 1 else 0)
+    println(if(a == b) 1 else 0)
+    println(if(a != b) 1 else 0)
+}
