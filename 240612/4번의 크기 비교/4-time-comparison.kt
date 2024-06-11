d0008ec8b9b2a45fc36f3ec9a9b10d6e95473c17
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-12 07:45:37 +0900

    Title: 4번의 크기 비교, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240612/4번의 크기 비교/4-time-comparison.kt
@@ -0,0 +1,26 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+
+var a = 0
+var b = 0
+var c = 0
+var d = 0
+var e = 0
+
+fun main() {
+    tokens = StringTokenizer(br.readLine())
+
+    a = tokens.nextToken().toInt()
+    b = tokens.nextToken().toInt()
+    c = tokens.nextToken().toInt()
+    d = tokens.nextToken().toInt()
+    e = tokens.nextToken().toInt()
+
+    println(if(a > b) 1 else 0)
+    println(if(a > c) 1 else 0)
+    println(if(a > d) 1 else 0)
+    println(if(a > e) 1 else 0)
+}
