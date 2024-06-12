497b209be0a36921a1046aa84ebecbbdef77a832
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-13 08:12:18 +0900

    Title: 차례로 출력, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240613/차례로 출력/print-in-order.kt
@@ -0,0 +1,23 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+
+var a = 0
+var b = 0
+var c = 0
+
+fun main() {
+   // tokens = StringTokenizer(br.readLine())
+    
+   // a = tokens.nextToken().toInt()
+   // b = tokens.nextToken().toInt()
+   // c = tokens.nextToken().toInt(
+    //a = br.readLine().toInt()
+
+    for(i in 5 .. 17)
+     print("$i ")
+
+   // println(if(a % 13 == 0 || a % 19 == 0) "True" else "False")
+}
