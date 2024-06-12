6ed9e143d3e7d7b7addfe32fb7c538ec543f1ed6
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-13 07:43:09 +0900

    Title: 물의 상태, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240613/물의 상태/state-of-water.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static StringTokenizer tokens;
+  private static int a, b;
+
+  public static void main(String[] args) throws IOException {
+    // tokens = new StringTokenizer(br.readLine());
+    // a = Integer.parseInt(tokens.nextToken());
+    // b = Integer.parseInt(tokens.nextToken());
+    a = Integer.parseInt(br.readLine());
+
+    if(a >= 100)
+        System.out.println("vapor");
+    else if(a >= 1)
+        System.out.println("water");
+    else
+        System.out.println("ice");
+  } //  main-end
+} //  Main-class-end
