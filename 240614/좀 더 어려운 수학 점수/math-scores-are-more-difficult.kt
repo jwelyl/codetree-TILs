762887b3b03cf6c1b47919ea8c49c719bdcf1bea
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-14 00:15:37 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240614/좀 더 어려운 수학 점수/math-scores-are-more-difficult.kt
@@ -0,0 +1,26 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+
+var a = 0
+var b = 0
+var c = 0
+var d = 0
+
+fun main() {
+   tokens = StringTokenizer(br.readLine())
+   a = tokens.nextToken().toInt()
+   b = tokens.nextToken().toInt()
+   
+   tokens = StringTokenizer(br.readLine())
+   c = tokens.nextToken().toInt(
+   d = tokens.nextToken().toInt(
+   
+   if(a == c)
+        println(if(b > d) "A" else "B")
+   else
+        println(if(a > c) "A" else "B")
+
+}
