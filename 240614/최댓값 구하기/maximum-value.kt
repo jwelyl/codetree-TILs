77f2b8849f6da5f0beebcb59e701e5214d3871e8
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-14 00:27:18 +0900

    Title: 최댓값 구하기, Time: 93ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240614/최댓값 구하기/maximum-value.kt
@@ -0,0 +1,19 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+
+var a = 0
+var b = 0
+var c = 0
+var max = 0
+
+fun main() {
+   tokens = StringTokenizer(br.readLine())
+   max = maxOf(max, tokens.nextToken().toInt())
+   max = maxOf(max, tokens.nextToken().toInt())
+   max = maxOf(max, tokens.nextToken().toInt())
+    
+   println(max)
+}
