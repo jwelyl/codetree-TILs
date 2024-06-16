48c8db7067ccf08e1569ac46ef336aa29482a1a5
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-16 21:13:55 +0900

    Title: b부터 a까지 감소, Time: 105ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240616/b부터 a까지 감소/dec-from-b-to-a.kt
@@ -0,0 +1,28 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+
+var a = 0
+var b = 0
+var c = 0
+var d = 0
+
+fun main() {
+    tokens = StringTokenizer(br.readLine())
+    a = tokens.nextToken().toInt()
+    b = tokens.nextToken().toInt()
+//     c = tokens.nextToken().toInt()
+    
+    // a = br.readLine().toInt()
+    // if(a > b && a > c)
+    //     println(if(b > c) b else c)
+    // else if(b > a && b > c)
+    //     println(if(a > c) a else c)
+    // else
+    //     println(if(a > b) a else b)
+
+    for(i in b downTo a)
+        print("$i ")
+}
