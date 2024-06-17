758cce234e0ba972f7790237e08ce247a65ef25f
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-18 00:29:28 +0900

    Title: 배수의 개수 2, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240618/배수의 개수 2/number-of-multipliers-2.kt
@@ -0,0 +1,50 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+var c = 0
+var d = 0
+
+fun main() {
+    // tokens = StringTokenizer(br.readLine())
+    // a = tokens.nextToken().toInt()
+    // b = tokens.nextToken().toInt()
+//     c = tokens.nextToken().toInt()
+    
+    // a = br.readLine().toInt()
+
+    // var i = a
+    // while(i <= b) {
+    //     print("$i ")
+
+    //     if(i % 2 == 1)
+    //         i *= 2
+    //     else
+    //         i += 3
+    // }
+
+    // for(i in 1 .. a) {
+    //     val str = i.toString()
+
+    //     if(str.indexOf("3") != -1 || str.indexOf("6") != -1 || str.indexOf("9") != -1 || i % 3 == 0)
+    //         print("0 ")
+    //     else
+    //         print("$i ")
+    // }
+
+    var ans = 0
+
+    repeat(10) {
+        val num = br.readLine().toInt()
+
+        if(num % 2 == 1)
+            ans++
+    }
+
+    print(ans)
+}
