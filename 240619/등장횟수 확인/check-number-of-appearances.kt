ae8b320b30f4e74e0a5d42e9d6e2f58ef818ba65
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-19 00:38:31 +0900

    Title: 등장횟수 확인, Time: 109ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240619/등장횟수 확인/check-number-of-appearances.kt
@@ -0,0 +1,48 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+var c = 0
+var d = 0
+
+fun main() {
+    // tokens = StringTokenizer(br.readLine())
+    // a = tokens.nextToken().toInt()
+    // b = tokens.nextToken().toInt()
+//     c = tokens.nextToken().toInt()
+    
+    // a = br.readLine().toInt()
+
+    // var i = a
+    // while(i <= b) {
+    //     print("$i ")
+
+    //     if(i % 2 == 1)
+    //         i *= 2
+    //     else
+    //         i += 3
+    // }
+
+    // for(i in 1 .. a) {
+    //     val str = i.toString()
+
+    //     if(str.indexOf("3") != -1 || str.indexOf("6") != -1 || str.indexOf("9") != -1 || i % 3 == 0)
+    //         print("0 ")
+    //     else
+    //         print("$i ")
+    // }
+
+    var ans = 0
+
+    repeat(5) {
+        if(br.readLine().toInt() % 2 == 0)
+            ans++
+    }
+
+    print("$ans")
+}
