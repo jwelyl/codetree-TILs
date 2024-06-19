948c82271940211c0aac051272ec244f95b77193
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-20 01:38:41 +0900

    Title: 주어진 수까지의 곱, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240620/주어진 수까지의 곱/multiplication-up-to-a-given-number.kt
@@ -0,0 +1,22 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+
+fun main() {
+    tokens = StringTokenizer(br.readLine())
+    a = tokens.nextToken().toInt()
+    b = tokens.nextToken().toInt()
+
+    var mult = 1
+
+    for(i in a .. b)
+        mult *= i
+
+    print("$mult")
+}
