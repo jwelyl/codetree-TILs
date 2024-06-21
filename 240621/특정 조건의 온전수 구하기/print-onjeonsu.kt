bff88eee29cb49f67da80201e49b6122ed74376d
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-21 09:52:54 +0900

    Title: 특정 조건의 온전수 구하기, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

--- 240621/특정 조건의 온전수 구하기/print-onjeonsu.kt
+++ 240621/특정 조건의 온전수 구하기/print-onjeonsu.kt
@@ -14,9 +14,9 @@ fun main() {
     var cnt = 0
 
     for(i in 1 .. a) {
-        if(i % 2 != 0 && i % 10 != 5 && !(i % 3 == 0 && i % 9 ! = 0))
-            sb.append("$i ")
+        if((i % 2 != 0) && (i % 10 != 5) && !(i % 3 == 0 && i % 9 != 0))
+        sb.append("$i ")
     }
-    
+
     println(sb)
 }
