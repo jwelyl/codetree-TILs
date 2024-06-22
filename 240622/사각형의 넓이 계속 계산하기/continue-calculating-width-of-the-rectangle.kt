53c9ad830f3e351743da7cebc69ce7afd72c1626
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-22 13:23:54 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240622/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.kt
@@ -0,0 +1,25 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+var c = '0'
+
+fun main() {
+    while(cnt < 3) {
+        tokens = StringTokenizer(br.readLine())
+        a = tokens.nextToken().toInt()
+        b = tokens.nextToken().toInt()
+        c = tokens,nextToken()[0]
+
+        sb.append(a * b).append("\n")
+        if(c == 'C')
+            break
+    }
+
+    print(sb)
+}
