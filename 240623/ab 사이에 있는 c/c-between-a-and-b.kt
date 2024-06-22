618a74c3a351aa4f855376c841bc8236075c0fc4
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-23 00:31:14 +0900

    Title: ab 사이에 있는 c, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240623/ab 사이에 있는 c/c-between-a-and-b.kt
@@ -0,0 +1,27 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+var c = 0
+
+fun main() {
+    // a = br.readLine().toInt()
+    tokens = StringTokenizer(br.readLine())
+    a = tokens.nextToken().toInt()
+    b = tokens.nextToken().toInt()
+    c = tokens.nextToken().toInt()
+
+    for(i in a .. b) {
+        if(i % c == 0) {
+            println("YES")
+            return
+        }
+    }
+
+    println("NO")
+}
