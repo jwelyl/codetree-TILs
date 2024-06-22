f63594d62e2124a19dfeed98075f3c1dde20b022
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-22 13:17:03 +0900

    Title: 출석 부르기 2, Time: 99ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240622/출석 부르기 2/calling-attendance-2.kt
@@ -0,0 +1,28 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+
+fun main() {
+    while(true) {
+        a = br.readLine().toInt()
+
+        if(a == 1)
+            sb.append("John\n")
+        else if(a == 2)
+            sb.append("Tom\n")
+        else if(a == 3)
+            sb.append("Paul\n")
+        else if(a == 4)
+            sb.append("Sam\n")
+        else
+            break
+    }
+
+    print(sb)
+}
