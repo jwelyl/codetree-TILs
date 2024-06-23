f4ff19e96a7eedda6147e125e68a541df052ebf6
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-24 00:30:14 +0900

    Title: 별표 출력하기, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240624/별표 출력하기/print-star.kt
@@ -0,0 +1,34 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+var c = 0
+
+fun main() {
+    // tokens = StringTokenizer(br.readLine())
+    // a = tokens.nextToken().toInt()
+    // b = tokens.nextToken().toInt()
+    // c = tokens.nextToken().toInt()
+
+    a = br.readLine().toInt()
+    
+    for(i in 1 .. a) {
+        repeat(i) {
+            sb.append("* ")
+        }
+        sb.append("\n")
+    }
+    for(i in a - 1 downTo 1) {
+        repeat(i) {
+            sb.append("* ")
+        }
+        sb.append("\n")
+    }
+
+    print(sb)
+}
