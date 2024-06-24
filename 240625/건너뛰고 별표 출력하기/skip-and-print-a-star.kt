31a678a6fb40ba7fe3e68fbf209c29f484dd9792
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-25 07:44:47 +0900

    Title: 건너뛰고 별표 출력하기, Time: 109ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240625/건너뛰고 별표 출력하기/skip-and-print-a-star.kt
@@ -0,0 +1,32 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+var c = 0
+
+fun main() {
+    // tokens = StringTokenizer(br.readLine())
+    // a = tokens.nextToken().toInt()
+    // b = tokens.nextToken().toInt()
+    // c = tokens.nextToken().toInt()
+
+    a = br.readLine().toInt()
+    
+    for(i in 1 .. a) {
+        for(j in 1 .. i)
+            sb.append("*")
+        sb.append("\n\n")
+    }
+    for(i in a - 1 downTo 1) {
+        for(j in 1 .. i)
+            sb.append("*")
+        sb.append("\n")
+    }
+
+    print(sb)
+}
