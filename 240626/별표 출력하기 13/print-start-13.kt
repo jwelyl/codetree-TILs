1b81e16c38f416e5785efee150a909dfb74b4e6c
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-26 00:22:55 +0900

    Title: 별표 출력하기 13, Time: 119ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240626/별표 출력하기 13/print-start-13.kt
@@ -0,0 +1,51 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+var c = 0
+
+fun main() {
+    // tokens = StringTokenizer(br.readLine())
+    // a = tokens.nextToken().toInt()
+    // b = tokens.nextToken().toInt()
+    // c = tokens.nextToken().toInt()
+
+    a = br.readLine().toInt()
+
+    for(i in 1 .. a) {
+        if(i % 2 == 0) {
+            repeat(i / 2) {
+                sb.append("* ")
+            }
+            sb.append("\n")
+        }
+        else {
+            repeat(a - (i - 1) / 2) {
+                sb.append("* ")
+            }
+            sb.append("\n")
+        }
+    }
+
+     for(i in a downTo 1) {
+        if(i % 2 == 0) {
+            repeat(i / 2) {
+                sb.append("* ")
+            }
+            sb.append("\n")
+        }
+        else {
+            repeat(a - (i - 1) / 2) {
+                sb.append("* ")
+            }
+            sb.append("\n")
+        }
+    }
+
+    print(sb)
+}
