2a014302fd2434f3f0645dd3ed647e933ee4ff32
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-26 00:28:40 +0900

    Title: 행 단위로 반복하여 출력, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240626/행 단위로 반복하여 출력/print-in-row.kt
@@ -0,0 +1,27 @@
+import java.util.*
+import java.io.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+lateinit var tokens : StringTokenizer
+val sb = StringBuilder()
+
+var a = 0
+var b = 0
+var c = 0
+
+fun main() {
+    // tokens = StringTokenizer(br.readLine())
+    // a = tokens.nextToken().toInt()
+    // b = tokens.nextToken().toInt()
+    // c = tokens.nextToken().toInt()
+
+    a = br.readLine().toInt()
+
+    for(i in 1 .. a) {
+       for(j in 1 .. a)
+         sb.append(j)
+       sb.append("\n")
+    }
+
+    print(sb)
+}
