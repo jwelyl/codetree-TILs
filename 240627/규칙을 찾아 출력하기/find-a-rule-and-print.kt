143cd2cb90732d50ac96079d300981e16c05331d
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-27 09:14:09 +0900

    Title: 규칙을 찾아 출력하기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240627/규칙을 찾아 출력하기/find-a-rule-and-print.kt
@@ -0,0 +1,38 @@
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+
+    private var n = 0
+
+    fun solve() {
+        n = br.readLine().toInt()
+
+        for(row in 1 .. n) {
+            if(row == 1 || row == n) {
+                repeat(n) {
+                    sb.append("* ")
+                }
+            }
+            else {
+                val cnt = row - 1
+                repeat(cnt) {
+                    sb.append("* ")
+                }
+                repeat(n - cnt - 1) {
+                    sb.append(" ")
+                }
+                sb.append("* ")
+            }
+
+            sb.append("\n")
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
