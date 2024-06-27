7e6cb2451f730dff07c3b10eccd2101b48f748b2
koreii <61617029+jwelyl@users.noreply.github.com>
2024-06-28 00:11:05 +0900

    Title: 정사각형 출력, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240628/정사각형 출력/print-square.kt
@@ -0,0 +1,33 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var a = 0
+    private var b = 0
+
+    fun solve() {
+        // tokens = StringTokenizer(br.readLine())
+        // a = tokens.nextToken().toInt()
+        // b = tokens.nextToken().toInt()
+
+        a = br.readLine().toInt()
+
+        for(row in 1 .. a) {
+            val start = a * (row - 1) + 1
+
+            repeat(a) {
+                sb.append("${start + it} ")
+            }
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
