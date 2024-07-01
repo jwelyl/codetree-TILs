c7e1eab1ab9b3635c10f0723bd903bb6e838e54c
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-01 09:24:32 +0900

    Title: 행에 따라 다르게 출력, Time: 95ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240701/행에 따라 다르게 출력/output-differently-for-row.kt
@@ -0,0 +1,36 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var a = 0
+    private var b = 0
+    private var cur = 9
+
+    fun solve() {
+        // tokens = StringTokenizer(br.readLine())
+        // a = tokens.nextToken().toInt()
+        // b = tokens.nextToken().toInt()
+
+        a = br.readLine().toInt()
+
+        for(row in 1 .. a) {
+            val add = if(row % 2 == 1) 1 else 2
+
+            for(col in 1 .. a) {
+                cur += add
+                sb.append("$cur ")
+            }
+            sb.append("\n")
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
