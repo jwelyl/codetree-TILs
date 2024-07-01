f82f6e58fb34bc3d7071fd62d9b29a7369db8731
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-02 01:07:46 +0900

    Title: 구구단 만들기 2, Time: 180ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240702/구구단 만들기 2/print-multiplication-table-2.kt
@@ -0,0 +1,34 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var a = 0
+    private var b = 0
+
+    fun solve() {
+        tokens = StringTokenizer(br.readLine())
+        a = tokens.nextToken().toInt()
+        b = tokens.nextToken().toInt()
+
+        for(right in 2 .. 8 step 2) {
+            for(left in b  downTo a) {
+                sb.append("$left * $right = ${left * right}")
+                
+                if(left != a)
+                    sb.append(" / ")
+                else
+                    sb.append("\n")
+            }
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
