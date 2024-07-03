59b597c28c501d005f66633ad898d775e51dcff4
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-04 00:09:02 +0900

    Title: 모양대로 숫자 출력하기, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240704/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.kt
@@ -0,0 +1,35 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var a = 0
+    private var b = 0
+
+    fun solve() {
+        // tokens = StringTokenizer(br.readLine())
+        // a = tokens.nextToken().toInt()
+        // b = tokens.nextToken().toInt()
+        a = br.readLine().toInt()
+
+        for(row in a downTo 1) {
+            repeat(a - row) {
+                sb.append("  ")
+            }
+
+            for(col in row downTo 1)
+                sb.append("$col ")
+
+            sb.append("\n")
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
