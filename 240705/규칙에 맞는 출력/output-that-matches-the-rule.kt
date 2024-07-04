12535dff33495129e567feef22c480fabf168afa
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-05 00:56:48 +0900

    Title: 규칙에 맞는 출력, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240705/규칙에 맞는 출력/output-that-matches-the-rule.kt
@@ -0,0 +1,32 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var a = 0
+    private var b = 0
+
+    private var num = 0
+
+    fun solve() {
+        // tokens = StringTokenizer(br.readLine())
+        // a = tokens.nextToken().toInt()
+        // b = tokens.nextToken().toInt()
+        a = br.readLine().toInt()
+
+        for(row in a downTo 1) {
+            for(col in row .. a)
+                sb.append("$col ")
+            sb.append("\n")
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
