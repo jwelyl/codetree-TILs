d74b3e2204cb8445175516e91e0156a0ee44679c
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-05 00:58:03 +0900

    Title: 일직선 구구단, Time: 99ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240705/일직선 구구단/one-line-multiple.kt
@@ -0,0 +1,31 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var a = 0
+    private var b = 0
+
+    private var num = 0
+
+    fun solve() {
+        // tokens = StringTokenizer(br.readLine())
+        // a = tokens.nextToken().toInt()
+        // b = tokens.nextToken().toInt()
+        a = br.readLine().toInt()
+
+        for(left in 1 .. a) {
+            for(right in 1 .. a)
+                sb.append("$left * $right = ${left * right}\n")
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
