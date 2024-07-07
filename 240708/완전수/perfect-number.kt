c15351bf2b2c62f63690c7e5b6f83ce4eb1c1a87
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-08 01:08:10 +0900

    Title: 완전수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240708/완전수/perfect-number.kt
@@ -0,0 +1,36 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var a = 0
+    private var b = 0
+    private var cnt = 0
+
+    fun solve() {
+        tokens = StringTokenizer(br.readLine())
+        a = tokens.nextToken().toInt()
+        b = tokens.nextToken().toInt()
+        
+        for(num in a .. b) {
+            var sum = 0
+
+            for(d in 1 .. num) [
+                if(num % d == 0)
+                    sum += d
+            ]
+
+            if(sum == 2 * num)
+                cnt++
+        }
+
+        println(cnt)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
