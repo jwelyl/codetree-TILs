97f405a70f6208a763c8864a5f184bfcf31aa277
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-10 00:42:14 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240710/특정 숫자 도달하기/reaching-specific-number.kt
@@ -0,0 +1,23 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var sum = 0
+    
+    fun solve() {
+        tokens = StringTokenizer(br.readLine())
+        repeat(10) {
+            sum += tokens.nextToken().toInt()
+        }
+
+        println(String.format("%d %.1f\n", sum, sum / 10.0)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
