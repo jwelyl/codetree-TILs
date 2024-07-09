228926520d250aa489eb66f48c245a6814870322
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-10 00:45:45 +0900

    Title: 특정 숫자 도달하기, Time: 95ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240710/특정 숫자 도달하기/reaching-specific-number.kt
+++ 240710/특정 숫자 도달하기/reaching-specific-number.kt
@@ -6,15 +6,28 @@ class Main {
     private val sb = StringBuilder()
     private lateinit var tokens : StringTokenizer
 
-    private var sum = 0
+    private var sum = 0.0
+    private var cnt = 0
+
+    private var ok = false
     
     fun solve() {
         tokens = StringTokenizer(br.readLine())
         repeat(10) {
-            sum += tokens.nextToken().toInt()
+            val num = tokens.nextToken().toInt()
+
+            sum += num
+            
+            if(num >= 250)
+                ok = true
+
+            if(it == 9 && ok) {
+                cnt = 9
+                sum -= num
+            } else cnt = 10
         }
 
-        println(String.format("%d %.1f\n", sum, sum / 10.0))
+        println(String.format("%d %.1f\n", sum.toInt(), sum / cnt))
     }
 }
 
