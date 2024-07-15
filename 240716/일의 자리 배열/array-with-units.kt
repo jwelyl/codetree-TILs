679227e9a70c03900505c00d8d71ab03643cf842
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-16 00:37:20 +0900

    Title: 일의 자리 배열, Time: 85ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240716/일의 자리 배열/array-with-units.kt
@@ -0,0 +1,32 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var f0 = 0
+    private var f1 = 0
+
+    fun solve() {
+        tokens = StringTokenizer(br.readLine())
+        f0 = tokens.nextToken().toInt()
+        f1 = tokens.nextToken().toInt()
+
+        sb.append("$f0 $f1 ")
+
+        repeat(8) {
+            val tmp = f0 + f1
+            f0 = f1
+            f1 = tmp
+            sb.append("$f1 ")
+        }
+        
+        println(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
