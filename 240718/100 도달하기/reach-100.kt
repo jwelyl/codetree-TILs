b3956b21e4a0180d58e8aa1ac7c4f5eb196e824d
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-18 00:27:03 +0900

    Title: 100 도달하기, Time: 625ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240718/100 도달하기/reach-100.kt
@@ -0,0 +1,36 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var n = 0
+    private var f1 = 1
+    private var f2 = 0
+    private var f = 0
+
+    fun solve() {
+        n = br.readLine().toInt()
+        f2 = n
+
+        sb.append("$f1 $f2 ")
+        while(true) {
+            f = f1 + f2
+            f1 = f2
+            f2 = f1
+
+            sb.append("$f ")
+
+            if(f > 100)
+                break
+        }
+
+        println(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
