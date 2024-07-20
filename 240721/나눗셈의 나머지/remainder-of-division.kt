1b053e4f85c5067c3ca447df0ff6650f1d894574
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-21 01:36:52 +0900

    Title: 나눗셈의 나머지, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240721/나눗셈의 나머지/remainder-of-division.kt
@@ -0,0 +1,34 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var a = 0
+    private var b = 0
+    private val cnts = IntArray(10)
+    private var sum = 0
+
+    fun solve() {
+        tokens = StringTokenizer(br.readLine())
+        a = tokens.nextToken().toInt()
+        b = tokens.nextToken().toInt()
+
+        while(a > 1) {
+            cnts[a % b]++
+
+            a %= b
+        }
+
+        for(i in 0 ..< 10)
+            sum += cnts[i] * cnts[i]
+
+        println(sum)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
