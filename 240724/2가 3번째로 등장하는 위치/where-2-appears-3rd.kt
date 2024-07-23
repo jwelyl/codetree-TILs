7a04f0729241c42e0a04a825697e1214d089e122
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-24 00:43:15 +0900

    Title: 2가 3번째로 등장하는 위치, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240724/2가 3번째로 등장하는 위치/where-2-appears-3rd.kt
@@ -0,0 +1,34 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb1 = StringBuilder()
+    private val sb2 = StringBuilder()
+    private lateinit var tokens : StringTokenizer
+
+    private var n = 0
+    private var cnt = 0
+
+    fun solve() {
+        n = br.readLine().toInt()
+
+        tokens = StringTokenizer(br.readLine())
+        for(i in 0 ..< n) {
+            val num = tokens.nextToken().toInt()
+
+            if(num == 2) {
+                cnt++
+
+                if(cnt == 3) {
+                    println(i + 1)
+                    return
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    Main().solve()
+}
