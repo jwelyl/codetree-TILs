9acd172be69f5e384a266c23bb13044833c5124f
koreii <61617029+jwelyl@users.noreply.github.com>
2024-07-30 00:09:46 +0900

    Title: 특정 원소들의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240730/특정 원소들의 합/sum-of-specific-elements.kt
@@ -0,0 +1,29 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var st : StringTokenizer
+
+    private var sum = 0
+
+    fun solve() {
+        for(i in 0 ..< 4) {
+            st = StringTokenizer(br.readLine())
+            
+            for(j in 0 ..< 4) {
+                val num = st.nextToken().toInt()
+
+                if(j <= i)
+                    sum += num
+            }
+        }
+
+        println(sum)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
