6d238ff31034973f8587d2dd3e5767f8331f4f98
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-02 00:16:43 +0900

    Title: 아래로 사각형 채우기, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240802/아래로 사각형 채우기/filling-rectangle-with-downflow.kt
@@ -0,0 +1,32 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var st : StringTokenizer
+
+    private val mat = Array(10) { IntArray(10) }
+
+    private var n = 0
+    
+    fun solve() {
+       n = br.readLine().toInt()
+
+        repeat(n) {
+            var num = it + 1
+
+            repeat(n) {
+                sb.append("num ")
+                num += n
+            }
+            sb.append("\n")
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
