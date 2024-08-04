3cfae95a35825da864717a0729c32ab6f644f002
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-05 00:29:06 +0900

    Title: 격자 모양 출력하기, Time: 95ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240805/격자 모양 출력하기/print-grid-shape.kt
@@ -0,0 +1,38 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var st: StringTokenizer
+
+    private val visited = Array(9) { IntArray(9) }
+    private var n = 0
+    private var m = 0
+
+    fun solve() {
+        st = StringTokenizer(br.readLine())
+        n = st.nextToken().toInt()
+        m = st.nextToken().toInt()
+    
+        repeat(m) {
+            st = StringTokenizer(br.readLine())
+            val y = st.nextToken().toInt()
+            val x = st.nextToken().toInt()
+
+            visited[y + 1][x + 1] = y * x
+        }
+
+        for(r in 0 until n) {
+            for(c in 0 until n)
+                sb.append("${visited[r][c]} ")
+            sb.append("\n")
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
