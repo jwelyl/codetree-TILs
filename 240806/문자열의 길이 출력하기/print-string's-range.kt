20488ba7b294032fdc82bdbee519ecf8551c8535
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-06 00:56:43 +0900

    Title: 문자열의 길이 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240806/문자열의 길이 출력하기/print-string's-range.kt
@@ -0,0 +1,16 @@
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    
+    fun solve() {
+        val input1 = br.readLine().length()
+        val input2 = br.readLine().length()
+
+        println(input1 + input2)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
