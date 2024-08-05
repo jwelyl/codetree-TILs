b09a0e5192b1538d4da6be75c5efe1dbdf46da04
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-06 00:53:34 +0900

    Title: 문자열 두번 출력하기, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240806/문자열 두번 출력하기/print-string-twice.kt
@@ -0,0 +1,16 @@
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    
+    fun solve() {
+        val input = br.readLine()
+
+        println(input)
+        println(input)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
