24e5d638172f84624e2ca1d342388c6383bd6c3c
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-08 01:11:53 +0900

    Title: 공백을 기준으로 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240808/공백을 기준으로 출력/output-based-on-space.kt
@@ -0,0 +1,20 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private lateinit var st : StringTokenizer
+    
+    fun solve() {
+        repeat(2) {
+            st = StringTokenizer(br.readLine())
+            sb.append(st.nextToken()).append(st.nextToken()).append("\n")
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
