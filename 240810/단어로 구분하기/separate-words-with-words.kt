cebfacad3af1fe812f6a2f787004a3da9c3a6fa8
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-10 00:32:00 +0900

    Title: 단어로 구분하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240810/단어로 구분하기/separate-words-with-words.kt
@@ -0,0 +1,19 @@
+import java.io.*
+import java.util.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var st : StringTokenizer
+
+    fun solve() {
+        while(st.hasMoreTokens())
+            sb.append(st.nextToken()).append("\n")
+        
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
