f8cb4620d09a23392b67ee6915f32634c37c781b
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-14 00:34:52 +0900

    Title: 문자열 추가하기, Time: 111ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240814/문자열 추가하기/add-spring.kt
@@ -0,0 +1,16 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var st: StringTokenizer
+
+    fun solve() {
+        println(br.readLine() + "Hello")
+    }
+}
+
+fun main() {
+    Main().solve()
+}
