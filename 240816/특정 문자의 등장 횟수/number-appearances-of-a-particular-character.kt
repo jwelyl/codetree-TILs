456cf1cb3ee46752a1fee5f7e08cceeef6707bd7
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-16 01:46:08 +0900

    Title: 특정 문자의 등장 횟수, Time: 987ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240816/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.kt
@@ -0,0 +1,49 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var st: StringTokenizer
+
+    private var cnt1 = 0
+    private var cnt2 = 0
+
+    fun solve() {
+        val input = br.readLine()
+
+        var idx = 0
+        while(true) {
+            val partial = input.substring(idx)
+            // println("partial = $partial")
+
+            val pos = partial.indexOf("ee")
+            
+            if(pos == -1)
+                break
+
+            cnt1++
+            idx = pos + 1
+        }
+
+        idx = 0
+        while(true) {
+            val partial = input.substring(idx)
+            // println("partial = $partial")
+
+            val pos = partial.indexOf("eb")
+            
+            if(pos == -1)
+                break
+
+            cnt2++
+            idx = pos + 1
+        }
+
+        println("$cnt1 $cnt2")
+    }
+}
+
+fun main() {
+    Main().solve()
+}
