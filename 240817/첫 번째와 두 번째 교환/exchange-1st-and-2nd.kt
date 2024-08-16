dca9ff86679ec8afe533fb7a2e40624ca26fbdfe
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-17 00:44:17 +0900

    Title: 첫 번째와 두 번째 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240817/첫 번째와 두 번째 교환/exchange-1st-and-2nd.kt
@@ -0,0 +1,29 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var st: StringTokenizer
+
+    private lateinit var str : CharArray
+
+    fun solve() {
+        str = br.readLine().toCharArray()
+        val first = str[0]
+        val second = str[1]
+
+        for(i in 0 ..< str.size) {
+            if(str[i] == first)
+                str[i] = second
+            else if(str[i] == second)
+                str[i] = first
+        }
+
+        println(String(str))
+    }
+}
+
+fun main() {
+    Main().solve()
+}
