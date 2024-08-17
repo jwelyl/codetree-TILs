561a8edde4670784362f7e7ffc5e19551d8f9efc
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-18 01:24:26 +0900

    Title: 문자열 놀이, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240818/문자열 놀이/play-with-string.kt
@@ -0,0 +1,48 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private val sb = StringBuilder()
+    private lateinit var st: StringTokenizer
+
+    private lateinit var str : CharArray
+    private var q = 0
+
+    fun solve() {
+        st = StringTokenizer(br.readLine())
+        str = st.nextToken().toCharArray()
+        q = st.nextToken().toInt()
+
+        repeat(q) {
+            st = StringTokenizer(br.readLine())
+            val option = st.nextToken().toInt()
+
+            if(option == 1) {
+                val a = st.nextToken().toInt() - 1
+                val b = st.nextToken().toInt() - 1
+
+                val tmp = str[a]
+                str[a] = str[b]
+                str[b] = tmp
+            }
+            else {
+                val from = st.nextToken()[0]
+                val to = st.nextToken()[0]
+
+                for(i in 0 until str.length) {
+                    if(str[i] == from)
+                        str[i] = to
+                }
+            }
+
+            sb.append(String(str))
+        }
+
+        print(sb)
+    }
+}
+
+fun main() {
+    Main().solve()
+}
