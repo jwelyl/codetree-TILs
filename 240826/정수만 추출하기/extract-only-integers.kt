86e7dc1a7bbde49c7b271967cb6c4c8e76b45872
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-26 01:42:25 +0900

    Title: 정수만 추출하기, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240826/정수만 추출하기/extract-only-integers.kt
@@ -0,0 +1,38 @@
+import java.util.*
+import java.io.*
+import kotlin.math.abs
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private lateinit var st: StringTokenizer
+
+    private val num1 = StringBuilder()
+    private val num2 = StringBuilder()
+    private var str1 = ""
+    private var str2 = ""
+
+    fun solve() {
+        st = StringTokenizer(br.readLine())
+
+        str1 = st.nextToken()
+        str2 = st.nextToken()
+
+        for(ch in str1) {
+            if(ch !in '0' .. '9')
+                break
+            num1.append(ch)
+        }
+
+        for(ch in str2) {
+            if(ch !in '0' .. '9')
+                break
+            num2.append(ch)
+        }
+
+        println(num1.toString().toInt() + num2.toString().toInt())
+    }
+}
+
+fun main() {
+    Main().solve()
+}
