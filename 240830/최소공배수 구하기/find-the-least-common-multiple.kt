600a6b77240e3a8b558fe49a35e525717e2b6f47
koreii <61617029+jwelyl@users.noreply.github.com>
2024-08-30 01:12:27 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240830/최소공배수 구하기/find-the-least-common-multiple.kt
@@ -0,0 +1,23 @@
+import java.util.*
+import java.io.*
+
+class Main {
+    private val br = BufferedReader(InputStreamReader(System.`in`))
+    private lateinit var st : StringTokenizer
+
+    private var n = 0
+    private var m = 0
+
+    fun solve() {
+        st = StringTokenizer(br.readLine())
+        n = st.nextToken().toInt()
+        m = st.nextToken().toInt()
+
+        for(i in 1 .. n * m) {
+            if(i % n == 0 && i % m == 0) {
+                println(i)
+                break
+            }
+        }
+    }
+}
