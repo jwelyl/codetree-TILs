9bb9f308a05bfb6a6f117a59cd591b81d0d6b71e
koreii <61617029+jwelyl@users.noreply.github.com>
2024-09-16 01:28:37 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240916/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    }
+}import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	private static int ans = 0;
+	
+	public static void main(String[] args) throws IOException {
+		recursion(Integer.parseInt(br.readLine()));
+	}	//	main-end
+	
+	private static void recursion(int num) {
+		if(num == 1) {
+			System.out.println(ans);
+			return;
+		}
+		
+		ans++;
+		
+		if(num % 2 == 0)
+			recursion(num / 2);
+		else
+			recursion(num * 3 + 1);
+	}
+}	//	Main-class-end
