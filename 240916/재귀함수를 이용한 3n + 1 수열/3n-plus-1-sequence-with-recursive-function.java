30527d7b9c5535b0de781af64ea9e8db23af83cd
koreii <61617029+jwelyl@users.noreply.github.com>
2024-09-16 01:28:54 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 240916/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.java
+++ 240916/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -1,8 +1,4 @@
-public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-    }
-}import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
