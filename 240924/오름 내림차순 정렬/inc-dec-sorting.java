f9865586c0138aa5bd757d703a0451e0af47a77e
koreii <61617029+jwelyl@users.noreply.github.com>
2024-09-24 00:52:30 +0900

    Title: 오름 내림차순 정렬, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240924/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final StringBuilder sb = new StringBuilder();
+	private static StringTokenizer st;
+	private static int N;
+	
+	private static final List<Integer> list = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++)
+			list.add(Integer.parseInt(st.nextToken()));
+		
+		Collections.sort(list);
+		for(int num : list)
+			sb.append(num).append("\n");
+		sb.append("\n");
+		Collections.sort(list, Collections.reverseOrder());
+		for(int num : list)
+			sb.append(num).append("\n");
+		sb.append("\n");
+		
+		System.out.print(sb);
+	} // main-end
+} // Main-class-end
